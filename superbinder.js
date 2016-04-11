2983563b09c8ef281b6659869ae8f81914186440
wryi <cherlton.millette@gmail.com>
2016-04-11 01:55:15 -0400

    Added extract function and supporting defineExrator function to allow easy extention of lib. Changed function index to deserialize and added a serialize function used by extract.

--- superbinder.js
+++ superbinder.js
@@ -1,52 +1,47 @@
 (function (window, $){
-	
+    
     var binders = {};
-    var mappers = {};
+    var extractors = {};
     
     var SuperBinder = function (formId){
-		this.form = formId;
-		this.success = function (response){
-			console.log(response);
-		};
+        this.form = formId;
+        this.success = function (response){
+            console.log(response);
+        };
 
-		this.error = function (error){
-			console.log(error);
-		};
+        this.error = function (error){
+            console.log(error);
+        };
 
-		setSubmitAction(this);
-	};
+        setSubmitAction(this);
+    };
 
     var defineBinder = SuperBinder.prototype.defineBinder = function (bindType, fn){
         if (typeof fn === 'function')
             return binders[bindType] = fn;
     };
 
-    var defineMapper = SuperBinder.prototype.defineMapper = function (mapType, fn){
-        if (typeof fn === 'function')
-            return mappers[mapType] = fn;
-    };
-
     defineBinder('text', function (element, data, bind) {
-        element.val(index(data, bind));
+        element.val(deserialize(data, bind));
     });
 
     defineBinder('option', function (element, data, bind) {
-        element.val(index(data, bind));
+        element.val(deserialize(data, bind));
     });
 
     defineBinder('radio', function (element, data, bind) {
-        if (index(data, bind))
+        if (deserialize(data, bind))
             return element.attr('checked', true);
     });
 
-	SuperBinder.prototype.bind = function (data) {
-		var elements = $(this.form).find('.binder');
+    SuperBinder.prototype.bind = function (data) {
+        var elements = $(this.form).find('.binder');
 
-		$.each(elements, function(){
-			var bind = $(this).attr('data-bind'),
-				type = $(this).attr('type') ? $(this).attr('type').toLowerCase(): null;
+        $.each(elements, function(){
+            var bind = $(this).attr('data-bind'),
+                type = $(this).attr('type') ? $(this).attr('type').toLowerCase(): null;
 
-			if (!bind){
+            if (!bind){
                 console.log('WARN: The data-bind attribute was not defined for an element of the class `bind`.');
             }   
             else if (!binders[type] && typeof(binders[type]) !== 'function') {
@@ -55,78 +50,130 @@
             else {
                 binders[type]($(this), data, bind);
             }
-    	});
-	};
+        });
+    };
 
-	SuperBinder.prototype.setSuccess = function (fn) {
-		if (typeof fn === 'function')
-			this.success = fn;
-	};
+    var defineExtractor = SuperBinder.prototype.defineExtractor = function (name, fn){
+        if (typeof fn === 'function')
+            return extractors[name] = fn;
+    };
 
-	SuperBinder.prototype.setError = function (fn) {
-		if (typeof fn === 'function')
-			this.error = fn;
-	};
+    defineExtractor('text', function (element, callback){
+        return callback((element.val() && element.val().length) > 0 ? element.val() : null);
+    });
 
-	function setSubmitAction(context) {
-		$(document).on('submit', context.form, function(e){
-			e.preventDefault();
+    defineExtractor('option', function (element, callback){
+        if (element.find(":selected").val())  
+            return callback(element.find(":selected").val().length > 0 ? element.find(":selected").val() : null);
+    });
 
-			context.url = $(this).attr('action');
-			context.method = $(this).attr('method');
+    defineExtractor('radio', function (element, callback){
+        if (element.attr("checked") == true)
+            return calllback(element.val());
+    });
 
-			var data = {},
-				binders = $(this).find('.binder');
+    defineExtractor('checkbox', function (element, callback) {
+        if (element.is( ":checked" ) )
+            callback(true);
+    });
 
-			$.each(binders, function (){
-				var bindType = $(this).attr('type') ? $(this).attr('type').toLowerCase(): null;
+    SuperBinder.prototype.extract =  function () {
+        var elements = $(this.form).find('.binder'),
+            data = {};
 
-				switch(bindType){
-					case 'text':
-						index(data, $(this).attr('data-bind'), $(this).val().length > 0 ? $(this).val() : null);
-						break;
-					case 'option':
-						index(data, $(this).attr('data-bind'), $(this).find(":selected").val().length > 0 ? $(this).find(":selected").val() : null);
-						break;
-					case 'radio':
-						if($(this).attr("checked") == true)
-							index(data, $(this).attr('data-bind'), $(this).val());
-						break;
-					default:
-						index(data, $(this).attr('data-bind'), $(this).val());
-						break;
-				};
-			});
+        $.each(elements, function (){
+            var mapTo = $(this).attr('data-map') ? $(this).attr('data-map') : $(this).attr('data-bind');
+            var bindType = $(this).attr('type') ? $(this).attr('type').toLowerCase(): null;
+            
+            if (extractors[bindType]) {
+                extractors[bindType]($(this), function (value){
+                    if (value) {
+                        serialize(data, mapTo, value);
+                    }
+                    else {
+                        console.log("WARN: The extractor function `" + bindType + "` yeild no data for element data-bind/data-map `" + mapTo + '`');
+                    }
+                });
+            } 
+            else {
+                console.log("WARN: No extractor function defined for type `" + bindType + "`")
+            }
+        });
 
-			$.ajax({
-				url: context.url,
-				type: context.method,
-				data: {data: JSON.stringify(data)},
-				success: context.success,
-				error: context.error
-			});
-		});
-	};
+        return data;
+    };
 
-	// Helper functions
-	//-------------------------------------------
-	function index(obj,is, value) {
-		if (typeof is == 'string') {
-			return index(obj, is.split('.'), value);
-		}
-		else if (is.length==1 && value!==undefined){
-			return obj[is[0]] = value;
-		}
-		else if (is.length==0) {
-			return jQuery.isEmptyObject(obj) ? null : obj;
-		}
-		else {
-			obj[is[0]] =  obj[is[0]] ? obj[is[0]] : {};
-			return index(obj[is[0]], is.slice(1), value);
-		}
-	}
+    function setSubmitAction(context) {
+        $(document).on('submit', context.form, function(e){
+            e.preventDefault();
+            context.url = $(this).attr('action');
+            context.method = $(this).attr('method');
 
-	// bind superbinder to the browser context.
-	if (window) window.SuperBinder = SuperBinder;
+            $.ajax({
+                url: context.url,
+                type: context.method,
+                data: context.extract(),
+                success: context.success,
+                error: context.error
+            });
+        });
+    };
+
+    SuperBinder.prototype.setSuccess = function (fn) {
+        if (typeof fn === 'function')
+            this.success = fn;
+    };
+
+    SuperBinder.prototype.setError = function (fn) {
+        if (typeof fn === 'function')
+            this.error = fn;
+    };
+
+    // Helper functions
+    //-------------------------------------------
+    function deserialize(obj,is, value) {
+        if (typeof is == 'string') {
+            return deserialize(obj, is.split('.'), value);
+        }
+        else if (is.length==1 && value!==undefined){
+            return obj[is[0]] = value;
+        }
+        else if (is.length==0) {
+            if (typeof obj === 'object' && jQuery.isEmptyObject(obj))
+                return null;
+            return obj;
+        }
+        else {
+            if (value) {
+                obj[is[0]] =  obj[is[0]] ? obj[is[0]] : {};
+            }
+
+            return deserialize(obj[is[0]], is.slice(1), value);
+        }
+    }
+
+    function serialize(obj, is, value) {
+        if (!value || !is)
+            return; // do not serialize null values.
+
+        if (typeof is == 'string') {
+            return serialize(obj, is.split('.'), value);
+        }
+        else if (is.length==1 && value!==undefined){
+            return obj[is[0]] = value;
+        }
+        else if (is.length==0) {
+            if (typeof obj === 'object' && jQuery.isEmptyObject(obj))
+                return null;
+            return obj;
+        }
+        else {
+            obj[is[0]] =  obj[is[0]] ? obj[is[0]] : {};
+            return serialize(obj[is[0]], is.slice(1), value);
+        }
+    }
+
+    // bind superbinder to the browser context.
+    if (window) window.SuperBinder = SuperBinder;
 
 })(window, jQuery);
